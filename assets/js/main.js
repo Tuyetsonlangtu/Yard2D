f590ee040ceb96d558d50f3ecabff2eee8df84b4
lam.hong <hongduclamvt>
2016-07-14 14:05:52 +0700

    er

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,42 @@
-var $DOUYardLayoutView = null;
+var canvas = null;
+var rate = 50;
+$(function () {
+    canvas = new fabric.Canvas('c1');
+    canvas.renderOnAddRemove = false;
+    drawBlock();
+    canvas.renderAll();
 
-$(function(){
-    $("#col2").DOUYardLayoutView({
-        width : 800,
-        height : 600
-    });
+    ///////////////////////////////////////////////
+    function drawBlock() {
+        var x = 0, y = 0;
+        for(var bayIdx in data.bays){
+            var bay = data.bays[bayIdx];
+           if(bayIdx != '7' && bayIdx != '39'){
+                y = 0;
+                for(var i = 0; i< bay.cells.length; i++){
+                    var cell = bay.cells[i];
+                    drawCell(x, y, zoom(cell.width), zoom(cell.length));
+                    y+= zoom(cell.length);
+                }
+                x+= zoom(bay.width);
+            }
+        }
+    }
 
-    $DOUYardLayoutView = $('#col2').data('DOUYardLayoutView');
-    $DOUYardLayoutView.drawShape()
+    function zoom(val){
+        return val * rate;
+    }
 
+    function drawCell(x, y, w, h){
+        var rect = new fabric.Rect({
+            left: x,
+            top: y,
+            width: w,
+            height: h,
+            fill:'',
+            stroke:'red',
+            strokeWidth:1
+        });
+        canvas.add(rect);
+    }
 });
